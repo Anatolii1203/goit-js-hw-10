5478d92134f6f8e49670bec8b5672c93968f38b0
Anatolii1203 <v120386t@gmail.com>
2024-05-21 18:45:49 +0300

    goit10-first

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,44 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+function createNotificationPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stateInput = form.querySelector('input[name="state"]:checked');
+
+  const delay = parseInt(delayInput.value);
+  const state = stateInput.value;
+
+  const notificationPromise = createNotificationPromise(delay, state);
+
+  notificationPromise.then(
+    delay => {
+      iziToast.success({
+        title: 'Fulfilled promise',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    },
+
+    delay => {
+      iziToast.error({
+        title: 'Rejected promise',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    }
+  );
+});
